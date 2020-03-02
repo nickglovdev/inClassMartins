a6c00f2bd581d5f238fa5b205cabf7ed730decab
nickglovdev <nickglovdev@gmail.com>
2020-03-02 17:32:45 -0600

    Added a new way to list out the fish depending on their sizes. There are 3 different types now for Martin.

--- scripts/fish/Fish.js
+++ scripts/fish/Fish.js
@@ -5,14 +5,12 @@ const Fish = (fishObject) => {
                 <img class="fish__image" src="${fishObject.image}" alt="">
             </div>
             <div class="fish__name">${fishObject.name}</div>
-            <button id="button--${fishObject.name}">Details</button>
-
+            <button class="fishButton" id="button--${fishObject.name}">Details</button>
             <dialog class="dialog--fish" id="details--${fishObject.name}">
                 <div>Species: ${fishObject.species}</div>
                 <div>Location: ${fishObject.location}</div>
-                <div>Length: ${fishObject.size}</div>
+                <div>Length: ${fishObject.length}</div>
                 <div>Food: ${fishObject.food.join(",")}</div>
-
                 <button class="button--close">Close Dialog</button>
             </dialog>
         </div>
